db594832fda1e2e945ad0ab7163dd6d4f71d6ce9
rw <nikunjbhanderi135@gmail.com>
2022-12-20 11:49:15 +0530

    textfilde

---
+++ android/app/src/main/kotlin/com/example/name_textfilde/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.name_textfilde
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:name_textfilde/HomePage.dart';
+
+void main()
+{
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    ),
+  );
+}
